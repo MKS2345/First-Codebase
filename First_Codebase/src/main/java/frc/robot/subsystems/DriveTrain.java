a76ce04ffb0640fa47654b02efbdebf19350ce55
Matthew Spratlin <mkspratlin01@bvsd.org>
2023-11-25 18:35:30 -0600

    a

--- First_Codebase/src/main/java/frc/robot/subsystems/DriveTrain.java
+++ First_Codebase/src/main/java/frc/robot/subsystems/DriveTrain.java
@@ -1,14 +1,14 @@
 package frc.robot.subsystems;
 
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
-import edu.wpi.first.wpilibj2.command.CommandBase;
+imprt edu.wpi.first.wpilibj2.command.CommandBase;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.wpilibj2.command.Subsystem;
 import frc.robot.Constants;
 
 public class DriveTrain implements Subsystem {
     private fi TalonSRX motorLeft1 = new TalonSRX(Constants.MOTOR_LEFT_1_ID);
-    private final TalonSRX motorLeft2 = new TalonSRX(Constants.MOTOR_LEFT_2_ID);
+    prvate final TalonSRX motorLeft2 = new TalonSRX(Constants.MOTOR_LEFT_2_ID);
     private final TalonSRX motorRight1 = new TalonSRX(Constants.MOTOR_RIGHT_1_ID);
     private final TalonSRX motorRight2 = new TalonSRX(Constants.MOTOR_RIGHT_2_ID);
 
@@ -16,9 +16,9 @@ public class DriveTrain implements Subsystem {
         motorLeft1.set(ControlMode.PercentOutput, speed);
         motorLeft2.set(ControlMode.PercentOutput, speed);
     }
-    public void setRightMotors(double speed) {
+    public vid setRightMotors(double speed) {
         motorRight1.set(ControlMode.PercentOutput, speed);
-        motorRight2.set(ControlMode.PercentOutput, speed);
+        motoright2.set(ControlMode.PercentOutput, speed);
     }
 }
 
