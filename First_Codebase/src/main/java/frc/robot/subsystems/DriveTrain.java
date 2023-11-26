1906d9d840251eb211b858cae8c48117727b49ee
Matthew Spratlin <mkspratlin01@bvsd.org>
2023-11-25 19:05:49 -0600

    .

--- First_Codebase/src/main/java/frc/robot/subsystems/DriveTrain.java
+++ First_Codebase/src/main/java/frc/robot/subsystems/DriveTrain.java
@@ -1,39 +1,24 @@
 package frc.robot.subsystems;
 
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
-imprt edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj2.command.CommandBase;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.wpilibj2.command.Subsystem;
 import frc.robot.Constants;
 
 public class DriveTrain implements Subsystem {
-    private fi TalonSRX motorLeft1 = new TalonSRX(Constants.MOTOR_LEFT_1_ID);
-    prvate final TalonSRX motorLeft2 = new TalonSRX(Constants.MOTOR_LEFT_2_ID);
+    private final TalonSRX motorLeft1 = new TalonSRX(Constants.MOTOR_LEFT_1_ID);
+    private final TalonSRX motorLeft2 = new TalonSRX(Constants.MOTOR_LEFT_2_ID);
     private final TalonSRX motorRight1 = new TalonSRX(Constants.MOTOR_RIGHT_1_ID);
     private final TalonSRX motorRight2 = new TalonSRX(Constants.MOTOR_RIGHT_2_ID);
 
     public void setLeftMotors(double speed) {
         motorLeft1.set(ControlMode.PercentOutput, speed);
         motorLeft2.set(ControlMode.PercentOutput, speed);
-        SubmissionPublisher<String> publisher = new SubmissionPublisher<>();
-
-        MySubscriber subs = new MySubscriber();
-        publisher.subscribe(subs);
-
-
-        List<String> strs = getStrs();
-
-        System.out.println("Publishing Items to Subscriber");
-        strs.stream().forEach(i -> publisher.submit(i));
-
     }
-    public vid setRightMotors(double speed) {
+    public void setRightMotors(double speed) {
         motorRight1.set(ControlMode.PercentOutput, speed);
-        motoright2.set(ControlMode.PercentOutput, speed);
-    }
-
-    finally {
-
+        motorRight2.set(ControlMode.PercentOutput, speed);
     }
 }
 
